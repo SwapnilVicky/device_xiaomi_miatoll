93d27f5ea8536537590589932bc32e96082dda48
Shubham Singh <coolsks94@gmail.com>
2019-03-30 00:04:47 +0100

    miatoll: Add support to build both gapps and non-gapps build

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -26,3 +26,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Set this flag in build script
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
+# Use Gapps
+TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+endif
