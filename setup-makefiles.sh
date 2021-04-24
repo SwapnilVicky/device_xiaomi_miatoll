fc79e0645704bcf589ca0cef577b793e38dda660
sairam1411 <majgaonkars3@gmail.com>
2021-04-24 21:11:20 +0530

    sm6250-common: Fix setup-makefiles.sh script for regenerating vendor makefile
    Fixed the problem about "$DEVICE must be set before including this script!"
    Fixed the problem about "$VENDOR must be set before including this script!"
    
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -9,6 +9,9 @@ set -e
 
 INITIAL_COPYRIGHT_YEAR=2020
 
+DEVICE_COMMON=sm6250-common
+VENDOR=xiaomi
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
