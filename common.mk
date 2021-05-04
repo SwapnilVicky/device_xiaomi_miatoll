0b345cade1e1175513c4009a59552acb2eb8f18c
P.Adarsh Reddy <padarshr@codeaurora.org>
2021-05-04 15:29:23 +0530

    sm6250-common: Enforce android-S new launch debugfs restrictions
    Change-Id: I78878e1c4177c07d66b13e5659d4360252c5f87b

--- common.mk
+++ common.mk
@@ -108,6 +108,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
+# DebugFS
+PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
+
 # Display
 PRODUCT_PACKAGES += \
     disable_configstore \
