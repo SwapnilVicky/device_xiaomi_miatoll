351be4f00671dcf2513afd081f270a31415c35ac
sairam1411 <majgaonkars3@gmail.com>
2021-05-15 22:09:23 +0530

    Revert "sm6250-common: Import Keyhandler from Davinci"
    This reverts commit 555ecd7969be37d2915bdb56a37be01963287e03.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -201,9 +201,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
-# Side FP key for InputDispatcher to skip
-TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 96
-
 # Treble
 BOARD_VNDK_VERSION := current
 
