2a359f6793fbb34995a79a9220c057d190311f1a
Chris Fries <cfries@google.com>
2021-07-05 16:27:25 +0000

    miatoll: Set VENDOR_SECURITY_PATCH to platform
    Set the VENDOR_SECURITY_PATCH level to match the platform
    security patch level.  These are functionally the same.
    
    Bug: 125914443
    Change-Id: Ib77ab115d6faabd21958c34e2dae49ec96421fdd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-06-05
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
