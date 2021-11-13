53b28ae6f801e8df162674ed07910e20fd249ad1
sairam1411 <majgaonkars3@gmail.com>
2021-11-13 23:31:38 +0530

    sm6250-common: Enable more flags to fix build errors
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- common.mk
+++ common.mk
@@ -56,6 +56,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Build
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+RELAX_USES_LIBRARY_CHECK := true
+
 # Component override
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
