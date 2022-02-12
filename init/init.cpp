975fb4658638454d949853b6dccb2c8ad301bd2e
sairam1411 <majgaonkars3@gmail.com>
2022-02-12 23:01:15 +0530

    miatoll: Update fingerprint to February 2022
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- init/init.cpp
+++ init/init.cpp
@@ -60,8 +60,8 @@ void set_device_props(const std::string brand, const std::string device, const s
 }
 
 void load_device_properties() {
-    property_override("ro.build.fingerprint", "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys");
-    property_override("ro.build.description", "raven-user 12 SQ1D.220105.007 8030436 release-keys");
+    property_override("ro.build.fingerprint", "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys");
+    property_override("ro.build.description", "raven-user 12 SQ1D.220205.003 8069835 release-keys");
     std::string hwname = GetProperty("ro.boot.hwname", "");
     std::string region = GetProperty("ro.boot.hwc", "");
 
