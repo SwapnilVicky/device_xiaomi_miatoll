a0368cd415c26fe0a204c14ccee0da6bca735760
kssrao13882 <kssrao13882@gmail.com>
2022-03-31 07:03:28 +0000

    sm6250-common: Import pixel_2016_exclusive.xml
    This allows Unlimited Backup in Google Photos in any Quality

--- common.mk
+++ common.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.1.vendor
+    
+# Google Photos
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
 # Health
 PRODUCT_PACKAGES += \
