ef425d750764a774aefa0e6014b5b3b2da5f3ebf
Pratyay Borborah <102370202+popoA3M@users.noreply.github.com>
2022-05-14 10:28:01 +0530

    miatoll: Switch to proton clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,8 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 TARGET_KERNEL_CONFIG := cust_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/miatoll
 
