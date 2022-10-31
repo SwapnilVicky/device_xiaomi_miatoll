dc78dd7a7c73dcbf5881100d0b3c8e2a6dd25be3
Sonal Singh <sonal.singh.19993@gmail.com>
2022-10-31 15:33:17 +0000

    [BLOBS] sm6250-common: Update blobs from V12.5.6.0.RJWMIXM [1/2]
     - from CURTANAGlobal V12.5.6.0.RJWMIXM
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,7 +192,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-06-01
+VENDOR_SECURITY_PATCH := 2022-05-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
