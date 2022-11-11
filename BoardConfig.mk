25809bbd44e014ca83564d57928d1ba842180043
Alexander Koskovich <akoskovich@pm.me>
2022-11-11 12:36:11 +0530

    miatoll: Remove virtual framebuffer
    - It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,6 @@ BOARD_KERNEL_CMDLINE += msm_rtb.enabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
