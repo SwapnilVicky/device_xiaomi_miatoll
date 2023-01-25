971e0232cf5dddd2b6e25b6698f814434a321c8d
SwapnilVicky <swapnil.motoe@gmail.com>
2023-01-25 10:58:19 +0530

    sm6250-common: Adapt overlays for lineage

--- common.mk
+++ common.mk
@@ -320,7 +320,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-bliss
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
