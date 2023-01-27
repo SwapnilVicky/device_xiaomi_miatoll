9517c05745d8811745b1cf5e599555a38f026606
SwapnilVicky <swapnil.motoe@gmail.com>
2023-01-27 14:54:04 +0530

    sm6250-common: Include MiuiCamera if exists
    Signed-off-by: Swapnil Vicky <swapnil.motoe@gmail.com>

--- common.mk
+++ common.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# Inherit MiuiCamera if exists
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
 # Enforce native interfaces of product partition as VNDK
 PRODUCT_PRODUCT_VNDK_VERSION := current
 
