7d33953b1b7c27d24bfa0737cc451937baf365fb
ChihYao Chien <ccchien@google.com>
2023-02-04 15:57:50 +0800

    miatoll: Add qti-telephony-hidl-wrapper-prd
    Provide qti-telephony-hidl-wrapper-prd for modules in product partition to include.
    
    Bug: 189177913
    Bug: 189905530
    [clarencelol: Add qti-telephony-utils-prd variant to match MIUI13]
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- device.mk
+++ device.mk
@@ -293,8 +293,11 @@ PRODUCT_PACKAGES += \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
+    qti-telephony-utils-prd \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
