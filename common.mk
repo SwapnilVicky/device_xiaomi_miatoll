a3856949e3c59e77c72c6d3080bdf8acaa874b59
SwapnilVicky <swapnil.motoe@gmail.com>
2023-03-12 18:13:30 +0530

    sm6250-common: audio: Revert all mess that's created earlier :)
    This reverts commit 109454368c4c644175c82528d77ac3f3615919fd.
    
    This reverts commit a7d2ddde64342770331ba9aeb21bacfff9bb2019.
    
    This reverts commit 9e778a5e2671103cf10ed93b1e03f236d9e2a3f4.
    
    This reverts commit 14ec521518526f9b2f37bd8a3ccaaf346c3dbf69.
    
    This reverts commit 83f314194a232207f8a3b03e42080a5095ee764f.
    
    This reverts commit 349fc4e82d480a64466ec19f4e8c0d34af604f98.
    
    This reverts commit 73d2dbca3443f31512c108dcf81f7bdb91396b24.
    
    This reverts commit 730c7d6831686a417b1a98f919a7fab7a668b46c.
    
    This reverts commit 96072ad3e849a6228937a9bf0e46477d81a4d897.
    
    This reverts commit afb05bb53462cf3d2926346063ef597f38cdb18c.
    
    This reverts commit 330278eb9f61317cdb53854564108cd15607a6b1.
    
    This reverts commit 3332fea6f89d12ba1f1a7c9a56ddc2977756d840.
    
    This reverts commit b3bcd10a10b6d4a4cac1c16e634c8d4c07203841.
    
    This reverts commit 75c2c0d62d62701bf93a929529c81c0f95c4862a.
    
    This reverts commit 9fea400ba21e9c9a5cb03d2919273567b1515a22.
    
    This reverts commit 837c8cf6d40f72c79533cec838f52ef78305bea0.

--- common.mk
+++ common.mk
@@ -50,7 +50,6 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
@@ -63,7 +62,9 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default
+    audio.bluetooth.default \
+    libbthost_if \
+    BluetoothQti
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
@@ -105,6 +106,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
+# Component override
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
