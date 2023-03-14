1fea1096b26ba8850ed7b4687d8bf040df3b9760
COSMIC <rayavanindra@gmail.com>
2023-03-14 16:09:24 +0530

    sm6250-common: Import new audio configuration from cosmic's tree
    -As the old configuration is kinda messed up so here we go
    
    Signed-off-by: Swapnil Vicky <swapnil.motoe@gmail.com>
    
    sm6250-common: audio: Set valid and supported channel mask for earpiece
    
    For earpiece to be properly enumerated as a output device,
    set valid and supported out_ channel mask.
    
    Change-Id: Ic83b009956ff43567b3acbb186d0069b16e91e83
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: COSMIC <rayavanindra@gmail.com>
    
    sm6250-common: audio: add compress recording configurations
    
    Add compress recording configurations
    
    CRs-Fixed: 2764160
    Change-Id: I8e8b1f89d7f28d997342c5946d0a35111e941ec1
    Signed-off-by: COSMIC <rayavanindra@gmail.com>
    
    sm6250-common: audio: fixup audio io policy voip_rx flags
    
    Flags for VOIP were mixed up as we move to CAF policy configuration. Amend the flags to get things working again.
    
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
    Change-Id: I2a2b202e2a58db9a26f571c091b1fa0b8f2949f6
    
    sm6250-common: audio: Don't advertise vorbis offloading support
    
    * Doesn't work at all.
    
    Change-Id: I8cd6341b240c3820dbbdef497e23e785b6ed99dc
    
    sm6250-common: audio: Update audio_io_policy for VoIP
    
    Add VoIP Tx app type config for pcm and low-latency captures.
    
    Bug: 133297310
    Test: make a voip call
    
    Change-Id: I5dd67167b7568fd08a7ae979b50417f2627dc7d9
    Signed-off-by: justinweng <justinweng@google.com>
    
    sm6250-common: audio: Offload 24 bits playback supports mp3/aac format
    
    compress_offload_24 supports mp3/aac format
    
    Bug: 132425237
    Test: mp3 playback
    
    Change-Id: I5742db45908f9d600cab0fef62badf447fc5c585
    Signed-off-by: justinweng <justinweng@google.com>
    
    sm6250-common: audio: Correct routing order for voip output
    
    Routing primary output before voip_rx output will cause unexpected
    RX/TX path suddenly and unmuted voice stream during when system
    plays touch sound and switches path between handset and handsfree.
    
    Bug: 152292858
    Test: manual test in VoIP
    Test: Regression test for VoIP(b/153276370) - pass
    
    Signed-off-by: Jasmine Cha <chajasmine@google.com>
    Change-Id: Ia60802ec887f0dfeaa0f93d151ba2f2c50e0d41d
    
    sm6250-common: audio: Fix mic issues in apps like WhatsApp
    
    * Also clean up FM Tuner from original commit as it breaks wired headphones
    
    -- fixed commit
    
    Change-Id: I311b484e07e41bf69ab43b78e4a9abe883730435
    
    sm6250-common: audio: Remove FM Tuner from input devices
    
    * This fixes google voice recorder.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: COSMIC <rayavanindra@gmail.com>
    
     sm6250-common: Move Bluetooth power overlays to sysprops
    
     * These were moved to sysprops in Android 13.
    
       https://android.googlesource.com/platform/system/libsysprop/+/976d33062b51e77edde44faad1a6e801c5dc0188%5E%21/#F0
    
    Change-Id: I5bff67a15c537e8a47a19f30776fca6bf2c9fbe5 | AOSP
    
    Signed-off-by: COSMIC <rayavanindra@gmail.com>
    
    sm6250-common: Switch BtAudio to AIDL
    
    Test: m
    Bug: 203490261 | AOSP
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7 | AOSP
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7 | AOSP
    
    Signed-off-by: COSMIC <rayavanindra@gmail.com>
    
    sm6250-common: import new audio & bluetooth configuration
    
    credit: https://github.com/Xtended-Devices/device_xiaomi_sm6250-common

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,50 +23,12 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#include <stdint.h>
-#include <string.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-int property_get(const char *key, char *value, const char *default_value);
-#ifdef __cplusplus
-}
-#endif
-
-#include "osi/include/osi.h"
-
-typedef struct {
-    const char *product_device;
-    const char *product_model;
-} device_t;
-
-static const device_t devices[] = {
-};
-
-static inline const char *BtmGetDefaultName()
-{
-    char product_device[92];
-    property_get("ro.product.device", product_device, "");
-
-    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
-        device_t device = devices[i];
-
-        if (strcmp(device.product_device, product_device) == 0) {
-            return device.product_model;
-        }
-    }
-
-    // Fallback to ro.product.model
-    return "";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-#define AVDT_NUM_SEPS 35
-
+// Skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
+// Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
+#define AVDT_NUM_SEPS 12
 #endif
