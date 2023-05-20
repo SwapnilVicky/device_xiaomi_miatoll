f7c02ac9632b3b810c943b7ac89221e4953d167f
SwapnilVicky <swapnil.motoe@gmail.com>
2023-05-20 09:29:25 +0530

    Revert "sm6250-common: Use on_vendor VNDK APEX"
    This reverts commit eaf1fd9340a6f782c028969e0b67d1712d92d16a.

--- device.mk
+++ device.mk
@@ -486,10 +486,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
-
-PRODUCT_PACKAGES += \
-    com.android.vndk.current.on_vendor
-
 PRODUCT_PACKAGES += \
     com.android.vndk.current.on_vendor
 
