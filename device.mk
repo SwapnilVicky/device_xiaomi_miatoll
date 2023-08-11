269e8df7eca1e5a0a3bb9f8373a42b1866483203
Swapnil Vicky <swapnil.motoe@gmail.com>
2023-08-11 15:11:54 +0530

    miatoll: Remove GoogleCameraGo
    Signed-off-by: SwapnilVicky <swapnil.motoe@gmail.com>

--- device.mk
+++ device.mk
@@ -78,8 +78,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    libgui_vendor \
-    GoogleCameraGo
+    libgui_vendor 
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
