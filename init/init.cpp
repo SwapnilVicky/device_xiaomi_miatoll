67f0a24f1fb46ad3b4323431815cdf5d194d244b
SwapnilVicky <swapnil.motoe@gmail.com>
2023-12-18 13:04:48 +0530

    miatoll: switch to stock fingerprints

--- init/init.cpp
+++ init/init.cpp
@@ -50,43 +50,59 @@ void set_ro_build_prop(const std::string &source, const std::string &prop,
     property_override(prop_name.c_str(), value.c_str(), false);
 }
 
-void set_device_props(const std::string brand, const std::string device, const std::string model) {
+void set_device_props(const std::string fingerprint, const std::string description,
+        const std::string brand, const std::string device, const std::string model) {
     for (const auto &source : ro_props_default_source_order) {
+        set_ro_build_prop(source, "fingerprint", fingerprint);
         set_ro_build_prop(source, "brand", brand, true);
         set_ro_build_prop(source, "device", device, true);
         set_ro_build_prop(source, "model", model, true);
     }
 
+    property_override("ro.build.fingerprint", fingerprint.c_str());
+    property_override("ro.build.description", description.c_str());
 }
 
 void load_device_properties() {
-    property_override("ro.build.fingerprint", "google/raven/raven:12/SQ1D.211205.017/7955197:user/release-keys");
-    property_override("ro.build.description", "raven-user 12 SQ1D.211205.016.A1 7957957 release-keys");
     std::string hwname = GetProperty("ro.boot.hwname", "");
     std::string region = GetProperty("ro.boot.hwc", "");
 
     if (hwname == "curtana") {
         if (region == "Global_TWO") {
             set_device_props(
+                    "Redmi/curtana_global/curtana:12/RKQ1.211019.001/V13.0.2.0.SJWMIXM:user/release-keys",
+                    "curtana_global-user 12 RKQ1.211019.001 V13.0.2.0.SJWMIXM release-keys",
                     "Redmi", "curtana", "Redmi Note 9S");
         } else if (region == "Global_PA") {
             set_device_props(
+                    "Redmi/curtana_global/curtana:12/RKQ1.211019.001/V13.0.2.0.SJWMIXM:user/release-keys",
+                    "curtana_global-user 12 RKQ1.211019.001 V13.0.2.0.SJWMIXM release-keys",
                     "Redmi", "curtana", "Redmi Note 9S");
         } else if (region == "India") {
             set_device_props(
+                    "Redmi/curtana/curtana:12/RKQ1.211019.001/V13.0.1.0.SJWINXM:user/release-keys",
+                    "curtana_in-user 12 RKQ1.211019.001 V13.0.1.0.SJWINXM release-keys",
                     "Redmi", "curtana", "Redmi Note 9 Pro");
         } else if (region == "Japan") {
             set_device_props(
+                    "Redmi/curtana_global/curtana:12/RKQ1.211019.001/V13.0.2.0.SJWMIXM:user/release-keys",
+                    "curtana_global-user 12 RKQ1.211019.001 V13.0.2.0.SJWMIXM release-keys",
                     "Redmi", "curtana", "Redmi Note 9S");
         }
     } else if (hwname == "excalibur") {
         set_device_props(
+                "Redmi/excalibur_in/excalibur:12/RKQ1.211019.001/V13.0.1.0.SJXINXM:user/release-keys",
+                "excalibur_in-user 12 RKQ1.211019.001 V13.0.1.0.SJXINXM release-keys",
                 "Redmi", "excalibur", "Redmi Note 9 Pro Max");
     } else if (hwname == "gram") {
         set_device_props(
+                "POCO/gram_in/gram:12/RKQ1.211019.001/V14.0.5.0.SJPINXM:user/release-keys",
+                "gram_in-user 12 RKQ1.211019.001 V14.0.5.0.SJPINXM release-keys",
                 "POCO", "gram", "POCO M2 Pro");
     } else if (hwname == "joyeuse") {
         set_device_props(
+                "Redmi/joyeuse_global/joyeuse:12/RKQ1.211019.001/V13.0.1.0.SJZMIXM:user/release-keys",
+                "joyeuse_global-user 12 RKQ1.211019.001 V13.0.1.0.SJZMIXM release-keys",
                 "Redmi", "joyeuse", "Redmi Note 9 Pro");
     }
 }
