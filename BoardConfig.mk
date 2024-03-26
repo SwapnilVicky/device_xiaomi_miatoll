b134a2725746b252deebdbe74c460dc14ad82dc8
xSylla <syllamodder@gmail.com>
2024-03-26 20:08:21 +0800

    miatoll: Enforce fstab suffix to qcom
    Change-Id: I5b7246196ec57020cdf980a4867b6b9b08ab3087 | AOSP
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,7 @@ TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
