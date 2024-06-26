da62f7247e3552c972f549d44383e8e5e06e616e
COSMIC <rayavanindra@gmail.com>
2024-06-26 13:52:50 +0530

    miatoll: Switch to QTI Thermal AIDL HAL
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- device.mk
+++ device.mk
@@ -321,11 +321,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.pixel \
-    thermal_symlinks
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+    android.hardware.thermal-service.qti
 
 # TimeKeep
 PRODUCT_PACKAGES += \
