b0c9dd8202446aa22583c4f128642ff2d2d3c06b
clarencelol <clarencekuiek@proton.me>
2024-07-20 21:16:17 +0800

    miatoll: libinit: Set default bluetooth device name using libinit
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -36,4 +36,6 @@ void set_variant_props(const variant_info_t variant) {
     property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
 
     property_override("ro.product.mod_device", variant.mod_device, true);
+
+    property_override("bluetooth.device.default_name", variant.model, true);
 }
