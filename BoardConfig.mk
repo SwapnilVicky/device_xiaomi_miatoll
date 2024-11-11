b9525c43e1b49910211451ca0c076c13db5d69db
Georg Veichtlbauer <georg@vware.at>
2024-11-11 23:47:43 +0800

    miatoll: Drop console from kernel cmdline
    Change-Id: Icb5765d2f97111abdaafc894bbd36d9b78c82f4e
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,6 @@ TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 KERNEL_CC := CC=clang
 
-BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
