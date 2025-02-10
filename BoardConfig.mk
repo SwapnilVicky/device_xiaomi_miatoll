e91d8e812ec83a511fbbc4da213d380529eb0d23
clarencelol <clarencekuiek@proton.me>
2025-02-11 00:29:50 +0800

    miatoll: Satisfy FCM checks
    Signed-off-by: clarencelol <clarencekuiek@proton.me>
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
