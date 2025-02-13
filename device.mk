0a03cb0dd85680c774b563ea0f57e38e39d47d9e
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:40:58 +0800

    miatoll: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`
    * Our kernel (4.14) does not support features needed for UFFD GC.
      Leaving it enabled results in long waiting time on the first boot.
    
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- device.mk
+++ device.mk
@@ -337,6 +337,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TimeKeep
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
