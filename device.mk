893c68be703f133619a0ce4b538c7f6943e32cda
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    miatoll: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- device.mk
+++ device.mk
@@ -53,6 +53,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
 
+$(call soong_config_set,camera,override_format_from_reserved,true)
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
