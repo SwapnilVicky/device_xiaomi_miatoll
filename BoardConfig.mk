535b4b4a48d537a97bcbf242e70d38fc0116ee3e
basamaryan <basam.aryan@gmail.com>
2025-10-08 11:59:44 -0700

    miatoll: Set kernel BPF version override to 5.4.299
    This matches android12-5.4
    
    Test: atest netd_integration_test
    Change-Id: I9b811c05610c11718d64289f10ab88c8c67df3c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,6 +143,7 @@ TARGET_BOARD_PLATFORM := atoll
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
